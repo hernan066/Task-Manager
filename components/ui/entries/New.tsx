01c77c9929acf6f0f805daec0afde869b5651e63
hernangit config --global user.name John <hernan0066@gmail.com>
2022-03-27 11:08:42 -0300

    New

--- components/ui/entries/New.tsx
+++ components/ui/entries/New.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { SetStateAction,  useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { entriesApi } from "../../../apis";
 import { Entry } from "../../../interfaces";
@@ -12,41 +12,17 @@ import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 
-const microOnVariants = {
-  initial: {
-    color: "red",
-    borderColor: "red",
-  },
-  animation: {
-    color: "#fff",
-    borderColor: "#fff",
-    transition: {
-      yoyo: Infinity,
-      duration: 0.6,
-    },
-  },
-};
-const microOffVariants = {
-  initial: {
-    color: "#fff",
-    border: "1px solid #fff",
-  },
-  
-  hover: {
-    border: "2px solid #fff",
-    transition: {
-      duration: 0.2,
-    },
-  },
-};
-
 const newVariants = {
-  initial: {opacity: 0, scale: 0.1, y: '-100%', x: '100%'},
-  animate: {opacity: 1, scale: 1, y: 0, x:0,   transition: { duration: 0.25 }},
-  exit: {opacity: 0, scale: 0.1, y: '-100%', x: '100%', transition: { duration: 0.25 }},
+  initial: { opacity: 0, scale: 0.1, y: "-100%", x: "100%" },
+  animate: { opacity: 1, scale: 1, y: 0, x: 0, transition: { duration: 0.25 } },
+  exit: {
+    opacity: 0,
+    scale: 0.1,
+    y: "-100%",
+    x: "100%",
+    transition: { duration: 0.25 },
+  },
 };
-
-
 
 export const New = () => {
   const newTask = useSelector((state: RootState) => state.ui.newTaskOpen);
@@ -102,76 +78,68 @@ export const New = () => {
   return (
     <>
       <div className="overlay-app"></div>
-    
-    <motion.div 
-    className="pop-up"
-    variants={newVariants}
-    initial="initial"
-    animate="animate"
-    exit="exit"
-    
-    >
-      <div className="pop-up__header">
-        <div className="pop-up__title">New Task</div>
 
-        <button className="pop-up-close" onClick={() => dispatch(closeNew())}>
-          <i className="fa-solid fa-xmark"></i>
-        </button>
-      </div>
+      <motion.div
+        className="pop-up"
+        variants={newVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+      >
+        <div className="pop-up__header">
+          <div className="pop-up__title">New Task</div>
 
-      <div className="pop-up__textarea-wrapper">
-        <textarea
-          className="pop-up__textarea"
-          autoFocus
-          placeholder="Add new task"
-          onChange={onTextFieldChanged}
-          value={inputValue}
-        ></textarea>
-      </div>
+          <button
+            className="pop-up-close"
+            onClick={() => (dispatch(closeNew()), handleStopListen())}
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </button>
+        </div>
 
-      <div className="content-button-wrapper">
-        <motion.button
-          /* whileHover={{ scale: 1.1 }}  */
-          whileTap={{ scale: 0.8 }}
-          transition={{ duration: 0.2 }}
-          className="new-button"
-          disabled={inputValue.length === 0 ? true : false}
-          onClick={onReset}
-        >
-          Clear
-        </motion.button>
-        <button
-          className="new-button"
-          disabled={inputValue.length === 0 ? true : false}
-          onClick={onSave}
-        >
-          Send
-        </button>
-      </div>
+        <div className="pop-up__textarea-wrapper">
+          <textarea
+            className="pop-up__textarea"
+            autoFocus
+            placeholder="Add new task"
+            onChange={onTextFieldChanged}
+            value={inputValue}
+          ></textarea>
+        </div>
 
-      {listening === true ? (
-        <motion.button
-          variants={microOnVariants}
-          initial="initial"
-          animate="animation"
-          className="pop-up-microphone-on"
-          onClick={() => handleStopListen()}
-        >
-          <i className="fa-solid fa-microphone"></i>
-        </motion.button>
-      ) : (
-        <motion.button
-          variants={microOffVariants}
-          initial="initial"
-          whileHover="hover"
-          className="pop-up-microphone-off"
-          onClick={() => handleStartListen()}
-        >
-          <i className="fa-solid fa-microphone"></i>
-        </motion.button>
-      )}
-    </motion.div>
+        <div className="content-button-wrapper">
+          <button
+            className="new-button"
+            disabled={inputValue.length === 0 ? true : false}
+            onClick={onReset}
+          >
+            Clear
+          </button>
+          <button
+            className="new-button"
+            disabled={inputValue.length === 0 ? true : false}
+            onClick={onSave}
+          >
+            Send
+          </button>
+        </div>
+
+        {listening === true ? (
+          <button
+            className="pop-up-microphone on"
+            onClick={() => handleStopListen()}
+          >
+            <i className="fa-solid fa-microphone"></i>
+          </button>
+        ) : (
+          <button
+            className="pop-up-microphone"
+            onClick={() => handleStartListen()}
+          >
+            <i className="fa-solid fa-microphone"></i>
+          </button>
+        )}
+      </motion.div>
     </>
-    
   );
 };
